7d97ac45e6091acb86ef2ec987d5df58dabebd4a
Pablo Perez <plperez@devcenter.es>
2018-05-02 10:56:37 +0200

    Change

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -10,12 +10,12 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
-
+// test
 class LuckyController
 {
     public function number()
     {
-        $number = mt_rand(0, 100);
+        $number = mt_rand(0, 100)*2;
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
